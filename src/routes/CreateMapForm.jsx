87cdd1676a89f6407383c18487f7d8ffdd22cdb4
Jerpancho <panchojerrold@gmail.com>
2022-10-28 13:18:21 -0500

    added react-router

---
+++ src/routes/CreateMapForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CreateMap() {
+	console.log('rendered');
+	return (
+		<div>
+			<p>test for create map form</p>
+		</div>
+	);
+}
+
+export default CreateMap;
