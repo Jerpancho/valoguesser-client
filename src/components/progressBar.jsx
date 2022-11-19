d52717b7d742d3ed5de8c012acf54ba369f4deb5
Jerpancho <panchojerrold@gmail.com>
2022-11-19 17:39:32 -0600

    added timeout to progress bar animation

--- src/components/progressBar.jsx
+++ src/components/progressBar.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import style from '../css/Progressbar.module.css';
-const progressBar = ({ amount = 500, total = 5000 }) => {
+const progressBar = ({ amount = 0, total = 5000 }) => {
 	const [fillStyle, setFillStyle] = useState({});
 	useEffect(() => {
 		const newWidth = Math.floor((amount / total) * 100);
-		setFillStyle({
-			width: `${newWidth}%`,
-		});
+		setTimeout(() => {
+			setFillStyle({
+				width: `${newWidth}%`,
+			});
+		}, 200);
 	}, [amount]);
 	return (
 		<div className={style.progress}>
