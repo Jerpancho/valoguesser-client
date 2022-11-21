5c5d9bf391761859ee8743adc597fd528dd08419
Jerpancho <panchojerrold@gmail.com>
2022-11-21 13:53:25 -0600

    added navigation to game over component

--- src/components/gameOver.jsx
+++ src/components/gameOver.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Map from './map';
-import style from '../css/GameOver.module.css';
+import PropTypes from 'prop-types';
+import ProgressBar from './progressBar';
+import style from '../css/Game.module.css';
+import { useNavigate } from 'react-router-dom';
 function gameOver({ state, rounds, gameState }) {
+	const [totalScore, setTotalScore] = useState(0);
+	const navigate = useNavigate();
+
+	function calculateTotalScore() {
+		let result = 0;
+		rounds.forEach((val) => {
+			result += val.score;
+		});
+		setTotalScore(result);
+	}
+
+	useEffect(() => {
+		calculateTotalScore();
+	}, []);
 	return (
 		<div className={style.gameOver}>
 			<Map mapData={state} rounds={rounds} gameState={gameState} width={500} height={500} />
+			<ProgressBar amount={totalScore} total={25000} width={700} />
+			<div className={style.gameOverButtons}>
+				<button
+					className={style.gameButton}
+					onClick={() => {
+						navigate('/');
+					}}
+				>
+					HOME
+				</button>
+				<button
+					className={style.gameButton}
+					onClick={() => {
+						window.location.reload(false);
+					}}
+				>
+					PLAY AGAIN
+				</button>
+			</div>
 		</div>
 	);
 }
 
+gameOver.propTypes = {
+	state: PropTypes.object,
+	rounds: PropTypes.array,
+	gameState: PropTypes.object,
+};
 export default gameOver;
--- src/components/progressBar.jsx
+++ src/components/progressBar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import style from '../css/Progressbar.module.css';
-const progressBar = ({ amount = 0, total = 5000 }) => {
+const progressBar = ({ amount = 0, total = 5000, width = 350 }) => {
 	const [fillStyle, setFillStyle] = useState({});
 	useEffect(() => {
 		const newWidth = Math.floor((amount / total) * 100);
@@ -11,7 +11,7 @@ const progressBar = ({ amount = 0, total = 5000 }) => {
 		}, 200);
 	}, [amount]);
 	return (
-		<div className={style.progress}>
+		<div className={style.progress} style={{ width: `${width}px` }}>
 			<span className={style.score}>{amount}</span>
 			<span className={style.total}>{total}</span>
 			<div className={style.progressFill} style={fillStyle}></div>
